d008fbaca5195850a4cc4d75f0547bc8ca32f073
Ben Smith <bnsmth235@gmail.com>
2018-02-16 08:58:29 -0700

    Employee Update
    Employee function update, actually makes it read your input

--- Gamer Stop Inventory/src/Inventory.java
+++ Gamer Stop Inventory/src/Inventory.java
@@ -1,4 +1,16 @@
+/*
+ * Simple UI for documenting an inventory for a fake
+ * game store that can be used as an employee or just a customer
+ * stores the name, price, system etc
+ * 
+ * @author Ben and Adam Smith
+ * @version 1.0
+ * @Since January 19, 2018
+ * 
+ */
+
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -26,48 +38,81 @@ public class Inventory {
 		e.setBounds(225,125,100,50); //where each button is positioned
 		c.setBounds(550,125,100,50);
 		
-		frame.add(title);
-		frame.add(e); //adding each of the components to the frame
-		frame.add(c);
+		frame.add(title); frame.add(e); frame.add(c);//adding each of the components to the frame
 		
 		c.addActionListener(new ActionListener(){ //if you press the customer button...
 			public void actionPerformed(ActionEvent e){
+				title.setVisible(false);
 				try {
-					start(); //goes to the start method
+					start();
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
-				}
+				} //goes to the start method
 			}
 		});
 		
 		e.addActionListener(new ActionListener(){ //if you press the employee button
 			public void actionPerformed(ActionEvent e){
 				
-				final JPasswordField value = new JPasswordField(); // 
-			    value.setBounds(100,75,100,30);   
+				frame.setVisible(false);//gets rid of old window
+				
+				JFrame login_frame = new JFrame("Employee Login");
+				login_frame.setSize(300, 300);
+				
+				login_frame.setLayout(null); //new window setup
+			    login_frame.setVisible(true);
+				
+				final JPasswordField pass = new JPasswordField(); //Password field text entry
+			    pass.setBounds(100,75,100,30);   
+			    
+			    final JTextField user = new JTextField();  
+			    user.setBounds(100,20, 100,30); //username entry
 			    
 			    JLabel l1=new JLabel("Username:");    
-			    l1.setBounds(20,20, 80,30);   
+			    l1.setBounds(20,20, 80,30);   //just username title
 			    
 			    JLabel l2=new JLabel("Password:");    
-			    l2.setBounds(20,75, 80,30);    
+			    l2.setBounds(20,75, 80,30);    //password title
 			    
 			    JButton b = new JButton("Login");  
-			    b.setBounds(100,120, 80,30);    
+			    b.setBounds(100,120, 80,30);   //login button 
 			    
-			    final JTextField text = new JTextField();  
-			    text.setBounds(100,20, 100,30);
+			    JButton r=new JButton("Register");
+			    r.setBounds(100, 140, 80, 30);
 			    
-			    frame.add(value); frame.add(l1); frame.add(l2); frame.add(b); frame.add(text);  
+			    
+			    
+			    login_frame.add(pass); login_frame.add(l1); login_frame.add(l2); login_frame.add(b); login_frame.add(user); //add all components to frame
+			    
 			    
 			    b.addActionListener(new ActionListener() {  
 			    	public void actionPerformed(ActionEvent e) {  
-			    		
-			    		String data = "Username " + text.getText();  
-			    		data += ", Password: "+ new String(value.getPassword());          
+			    		String username = user.getText();  
+			    		String password = pass.getText();
+
+			    		try {
+							if(FileReader(username, password)==true){
+								System.out.println("Welcome"+username);
+								JLabel welcome=new JLabel("Welcome "+username);
+								welcome.setBounds(100, 200, 80, 30);
+								login_frame.add(welcome);
+								System.out.println("Frame should be added");
+							}else{
+								System.out.println("return false try again");
+							}
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 			    	}  
-			    });   
+			    });  
+			    
+			    r.addActionListener(new ActionListener(){
+			    	public void actionPerformed(ActionEvent a){
+			    		
+			    	}
+			    });
 			}
 		});
 		
@@ -77,7 +122,15 @@ public class Inventory {
 	public static void start() throws IOException{
 		JFrame c_frame=new JFrame("Customer Inventory Search");
 		c_frame.setSize(1000, 500);
+		c_frame.setLayout(null);
 		c_frame.setVisible(true);
+		
+		JLabel c_start=new JLabel("Search for a Title by Name or Console");
+		c_start.setBounds(333,25,666,75);
+		
+		
+		c_frame.add(c_start);
+		
 		System.out.println("Enter a word or search for one,\nType \'Search\' or \'s\' to Search"
 				+ " or type \'New Title\' or \'nt\' to enter a Title and information.");
 		
@@ -89,7 +142,7 @@ public class Inventory {
 			
 			System.out.println("Search for Title:");
 			srch_in=in.next();
-			FileReader(srch_in);
+			//FileReader(srch_in);
 			
 			
 		}else if(Srch_NW.equals("nw")||
@@ -108,57 +161,30 @@ public class Inventory {
 	}
 	
 	public static void FileWriter(String word_and_def) throws IOException{
-		FileWriter fw=new FileWriter("E:\\School\\Programming\\Java\\TalkBot\\Saved Words.txt", true);
+		FileWriter fw=new FileWriter("C:\\Users\\bs034696\\Documents\\GitHub\\Gamer-Stop-Inventory\\Gamer Stop Inventory\\src\\User_pass.txt.txt", true);
 		PrintWriter pw=new PrintWriter(fw, true);
 		pw.println(word_and_def); //File Writing Method so I don't have to type this every time
 		pw.close();
 		fw.close();
-		System.out.println("Successfully Saved to Dictionary");
+		
 	}
 	
-	public static void FileReader(String word) throws IOException{
-		Scanner scan=new Scanner(new File("E:\\School\\Programming\\Java\\TalkBot\\Saved Words.txt"));
-		String word_list[]=new String[1000]; //reads the file up to 1000 lines (words&definitions)
-		int i=0;
+	public static boolean FileReader(String user, String pass) throws IOException{
+		System.out.println("file reader active");
+		Scanner scan=new Scanner(new File("C:\\Users\\bs034696\\Documents\\GitHub\\Gamer-Stop-Inventory\\Gamer Stop Inventory\\src\\User_pass.txt.txt"));
 		while(scan.hasNextLine()){
 			final String find_line=scan.nextLine();
-			if(find_line.toLowerCase().contains(word.toLowerCase())){//checks if the word (lower case) is in any words or 
-				word_list[i]=find_line;								//definitions (also lower case)
-				i++;
+			if(find_line.toLowerCase().contains(user.toLowerCase())){//checks if the word (lower case) is in any words or 
+				if(find_line.toLowerCase().contains(pass.toLowerCase())){
+					scan.close();
+					System.out.println("return true");
+					return true;
+				}
 			}
-			
-		}
-		
-		if(i-1==-1){ //checks if there are any entries containing the search
-			System.out.println("There are no entries containing your search, would you like to make a "
-					+ "new one? (yes/no)");
-			
-			String yes_no=in.next();
-			
-			if(yes_no.equals("yes")||
-					yes_no.equals("Yes")||
-					yes_no.equals("YES")||//Several options for user type
-					yes_no.equals("y")||
-					yes_no.equals("Y")){
-				
-				word_def_check(word);
-				new_wrd_not_found();//sends to new method in case they made a mistake and can loop
-				
-			}else{
-				System.exit(0);
-			}
-			
-		}else{
-			
-			System.out.println("The following contain the word(s) that you searched for:");
-			for(int j=0;j<=i-1;){		//iterates and prints through how many were found minus one for the NullPointer
-				System.out.println("\n"+word_list[j]);
-				j++;
-			}
-			new_wrd_not_found();
 		}
 		scan.close();
-		start();
+		System.out.println("return false");
+		return false;
 	}
 	
 	
